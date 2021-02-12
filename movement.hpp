61289c7fd94d34bce51e3350fae9d6e89705eaff
mike <75542806+SirMik3@users.noreply.github.com>
2021-02-12 11:09:17 +0200

    Update movement.hpp

--- movement.hpp
+++ movement.hpp
@@ -2,18 +2,19 @@
 #include <iostream>
 using namespace std;
 
-int movement(string str, double speed, double time, double pos, double angle)
+int movement(string str, double speed, double time, double pos, double angle, double jumptime)
 { 
 
 	double dx = 0;
 	double dy = 0;
 	double dz = 0;
+	double gravity = 9.8 * jumptime;
 if (str == "x"){
 	dx = (double) (cos(angle) * speed) / time;
 } else if (str == "y") {
 	dy = (double) (sin(angle) * speed) / time;
 } else {
-	dz = (double) (tan(angle) * speed) / time;
+	dz = ((double) (tan(angle) * speed) / time) - gravity;
 }
 
 	if (str == "x"){
