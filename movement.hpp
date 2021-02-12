34596cf3c8148c608708ed319135d785ab8b73da
mike <75542806+SirMik3@users.noreply.github.com>
2021-02-12 11:14:18 +0200

    Update movement.hpp

--- movement.hpp
+++ movement.hpp
@@ -8,7 +8,7 @@ int movement(string str, double speed, double time, double pos, double angle, do
 	double dx = 0;
 	double dy = 0;
 	double dz = 0;
-	double gravity = 9.8 * jumptime;
+	double gravity = 9.8 * time;
 if (str == "x"){
 	dx = (double) (cos(angle) * speed) / time;
 } else if (str == "y") {
